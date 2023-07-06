5d30728a0624597128c6d4b84d2c62b3505ae0ec
ksheck6 <ksheck@stefanini.com>
2023-07-06 14:44:54 -0300

    setando o banco de dados

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""""
+
+Calculator functions for the app
+
+"""""
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract two numbers"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+
+Sample tests file
+
+"""
+
+from django.test import TestCase
+
+from app import calc
+
+
+class CalcTests(TestCase):
+    """Tests for calculator app"""
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(calc.add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(calc.subtract(11, 5), 6)
